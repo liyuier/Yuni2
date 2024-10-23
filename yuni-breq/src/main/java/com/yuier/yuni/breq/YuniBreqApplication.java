326503addf6b130f6ef378d235cfeeb54168757a
liyuier <me@yuier.com>
2024-10-23 20:25:14 +0800

    初始化项目结构

---
+++ yuni-breq/src/main/java/com/yuier/yuni/breq/YuniBreqApplication.java
@@ -0,0 +1,13 @@
+package com.yuier.yuni.breq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuniBreqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuniBreqApplication.class, args);
+    }
+
+}
---
+++ yuni-breq/src/test/java/com/yuier/yuni/breq/YuniBreqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuier.yuni.breq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuniBreqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
