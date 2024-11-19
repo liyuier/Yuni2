d78f168a7104dc393d611cd3d77dc9953583497e
liyuier <me@yuier.com>
2024-11-19 23:37:46 +0800

    manager 暂且搁置，先把模式匹配器写好

---
+++ yuni-core/src/main/java/com/yuier/yuni/core/randosoru/bot/group/GroupManager.java
@@ -0,0 +1,15 @@
+package com.yuier.yuni.core.randosoru.bot.group;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Title: GroupManager
+ * @Author yuier
+ * @Package com.yuier.yuni.core.randosoru.bot.group
+ * @Date 2024/11/18 1:47
+ * @description: bot 的群组管理器
+ */
+
+@Component
+public class GroupManager {
+}
